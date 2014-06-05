80becc40e3814421b7237db17b63baa30721f0f3
Tony Armstrong <i.am@iamtonyarmstrong.com>
2014-06-04 23:34:48 -0400

    First Commit

---
+++ Bloggy/TableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TableViewController.h
+//  Bloggy
+//
+//  Created by Anthony Armstrong on 6/3/14.
+//  Copyright (c) 2014 iamtonyarmstrong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@property (nonatomic,strong)NSArray *titles;
+@property (nonatomic,strong)NSArray *blogPosts;
+
+@end
---
+++ README.md
@@ -0,0 +1,1 @@
+This app uses a basic UITableViewController to receive and display data from a RESTful interface that returns JSON representing blog post data.
